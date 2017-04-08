d7c15036a398e832246e9530ac9907f94d73f5c1
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2017-04-08 14:06:49 -0700

    Add beak - rotation

--- Assets/Game/States/BattleState/Battle/BattlePlayer.cs
+++ Assets/Game/States/BattleState/Battle/BattlePlayer.cs
@@ -16,7 +16,7 @@ namespace DT.Game.Battle {
 
 
 		// PRAGMA MARK - Internal
-		private const float kPlayerSpeed = 3.0f;
+		private const float kPlayerSpeed = 4.8f;
 
 		private Rigidbody rigidbody_;
 		private InputDevice inputDevice_;
@@ -29,7 +29,13 @@ namespace DT.Game.Battle {
 			Vector2 deltaPosition = inputDevice_.LeftStick.Value * Time.fixedDeltaTime * kPlayerSpeed;
 			// convert 2D coord -> 3D
 			Vector3 deltaWorldPosition = new Vector3(deltaPosition.x, 0.0f, deltaPosition.y);
+
 			rigidbody_.MovePosition(rigidbody_.position + deltaWorldPosition);
+
+			// snap rotation if input is not (0, 0)
+			if (deltaWorldPosition.magnitude > Mathf.Epsilon) {
+				rigidbody_.MoveRotation(Quaternion.LookRotation(deltaWorldPosition));
+			}
 		}
 	}
 }
