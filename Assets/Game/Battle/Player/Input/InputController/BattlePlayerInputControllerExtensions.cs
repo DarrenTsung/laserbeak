7e59aebe05439bdd6bd808f5bc886d9b2710084f
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2017-04-23 14:45:37 -0700

    Cleanup commented out AnimatedMovement code

--- Assets/Game/Battle/Player/Input/InputController/BattlePlayerInputControllerExtensions.cs
+++ Assets/Game/Battle/Player/Input/InputController/BattlePlayerInputControllerExtensions.cs
@@ -13,13 +13,8 @@ namespace DT.Game.Battle.Players {
 		public static void MoveTo(this BattlePlayerInputController controller, BattlePlayer player, Vector3 endPosition, float duration, EaseType easeType) {
 			controller.DisableInput(BattlePlayerInputController.PriorityKey.Movement);
 			Vector3 startPosition = player.Rigidbody.position;
-			Vector3 lastPosition = startPosition;
 			player.Rigidbody.AddForce((endPosition - startPosition) * 10, ForceMode.Impulse);
-			var coroutine = CoroutineWrapper.DoEaseFor(duration, easeType, (float p) => {
-				// Vector3 newPosition = Vector3.Lerp(startPosition, endPosition, p);
-				// player.Rigidbody.velocity = newPosition - player.Rigidbody.position;
-			}, () => {
-				// player.Rigidbody.velocity = Vector3.zero;
+			var coroutine = CoroutineWrapper.DoAfterDelay(duration, () => {
 				controller.EnableInput(BattlePlayerInputController.PriorityKey.Movement);
 				controller.CancelAnyAnimatedMovements();
 			});
